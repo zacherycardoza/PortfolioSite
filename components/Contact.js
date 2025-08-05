a22b98fdd2513c5c1d230ad782ced6d9c4ca2566
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-08-05 16:00:43 -0400

    Contact Form

--- components/Contact.js
+++ components/Contact.js
@@ -1,8 +1,46 @@
 export default function Contact() {
   return (
-    <section id="contact-section" className="p-8 min-h-dvh scroll-mt-[4rem]">
-      <h3 className="text-2xl font-semibold mb-4 text-center">Contact</h3>
-      <p>contact me</p>
+    <section
+      id="contact-section"
+      className="p-8 min-h-[400px] scroll-mt-[4rem]"
+    >
+      <h3 className="px-8 text-5xl font-semibold text-center">
+        <span className="">Contact</span>
+      </h3>
+      <div className="flex items-center justify-center p-6">
+        <form className="p-6 rounded-lg shadow-lg w-full max-w-md space-y-4">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            className="w-full p-2 border rounded"
+            required
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            className="w-full p-2 border rounded"
+            required
+          />
+
+          <textarea
+            name="message"
+            placeholder="Your Message"
+            rows="4"
+            className="w-full p-2 border rounded"
+            required
+          ></textarea>
+
+          <button
+            type="submit"
+            className="w-full accent-background text-white p-2 rounded hover:bg-red-700"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
