a7ab1b218513af894496b432fca41588951077a7
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-08-05 16:00:30 -0400

    About Section changes

--- components/About.js
+++ components/About.js
@@ -2,11 +2,13 @@ export default function About() {
   return (
     <section
       id="about-section"
-      className="p-8 min-h-dvh scroll-mt-[4rem] text-center"
+      className="p-8 min-h-[400px] scroll-mt-[4rem] text-center"
     >
-      <h3 className="text-2xl font-semibold mb-4 text-center">About Me</h3>
+      <h3 className="px-8 text-5xl font-semibold mb-4 text-center">
+        <span className="">About Me</span>
+      </h3>
       <div className="flex">
-        <div className="basis-1/3"></div>
+        <div className="basis-2/3"></div>
         <p>
           Hey, I’m Zach — a web developer who loves turning ideas into clean,
           functional websites and apps. I recently finished Centennial’s
@@ -16,7 +18,7 @@ export default function About() {
           I’m not coding, I’m probably outside, working on a DIY project, or
           fishing.
         </p>
-        <div className="basis-1/3"></div>
+        <div className="basis-2/3"></div>
       </div>
     </section>
   );
